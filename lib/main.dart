21dd01d69a1769fb4a13b9fd8c3f4b04ad7e361f
guilhermemoretto12 <guilhermemoretto12@gmail.com>
2022-09-05 19:32:40 -0300

    Setup Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app/my_app.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // FirebaseFirestore.instance.collection('students_warings').doc().set({
+  //   'student_id': 'YdviIbZRx1AbCbO4JpR9',
+  //   'warning_id': 'a5j17GBNG1SSfMHBwHeM',
+  //   'reading_date': '',
+  // });
 }
