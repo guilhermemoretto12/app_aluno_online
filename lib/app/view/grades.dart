b162285b97c7abbc444e126822b18b7cfb665d01
guilhermemoretto12 <guilhermemoretto12@gmail.com>
2022-09-19 22:56:41 -0300

    Fetch grades collection

--- lib/app/view/grades.dart
+++ lib/app/view/grades.dart
@@ -1,7 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Grades extends StatelessWidget {
+class Grades extends StatefulWidget {
   const Grades({Key? key}) : super(key: key);
+
+  @override
+  State<Grades> createState() => _GradesState();
+}
+
+class _GradesState extends State<Grades> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,6 +20,36 @@ class Grades extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Notas e Frequência'),
         ),
-        body: ListView());
+        body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+          stream: FirebaseFirestore.instance
+              .collection("grades")
+              .where("student_id", isEqualTo: "YdviIbZRx1AbCbO4JpR9")
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  return Text('Período: ' +
+                      snapshot.data!.docs[index].get('period').toString() +
+                      '\nMatéria: ' +
+                      snapshot.data!.docs[index].get('discipline') +
+                      '\nNota: ' +
+                      snapshot.data!.docs[index].get('grade').toString() +
+                      '\nAulas: ' +
+                      snapshot.data!.docs[index].get('classes').toString() +
+                      '\nFrequência: ' +
+                      snapshot.data!.docs[index].get('frequency').toString() +
+                      ' (${snapshot.data!.docs[index].get('frequency') * 100 / snapshot.data!.docs[index].get('classes')})%');
+                },
+              );
+            }
+            if (snapshot.hasError) {
+              return const Text('Error');
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ));
   }
 }
