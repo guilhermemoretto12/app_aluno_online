0e1076e07f101338401a7464bdd83e8a16bcff3b
guilhermemoretto12 <guilhermemoretto12@gmail.com>
2022-10-04 21:16:43 -0300

    Change app name and icon

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Aluno Online',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
