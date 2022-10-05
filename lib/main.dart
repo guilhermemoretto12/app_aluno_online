a7ca420051ddabd7768210c4360fc86623c3c26c
guilhermemoretto12 <guilhermemoretto12@gmail.com>
2022-10-04 21:17:04 -0300

    Fix null exception error on emulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app/my_app.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
-
-  // FirebaseFirestore.instance.collection('students_warings').doc().set({
-  //   'student_id': 'YdviIbZRx1AbCbO4JpR9',
-  //   'warning_id': 'a5j17GBNG1SSfMHBwHeM',
-  //   'reading_date': '',
-  // });
 }
